9d95bcd7b0ac16ec633a29b8e9d5ea45325bbcdb
Jose Rodriguez <jrodriguez@denwaip.com>
2024-04-21 15:23:59 -0300

    add new endpoints

--- src/incidents/incidents.controller.js
+++ src/incidents/incidents.controller.js
@@ -5,12 +5,65 @@ const Incident = require('../../models/incident.model');
 const Team = require('../../models/team.model');
 const Player = require('../../models/player.model');
 
+const findLast = async () => {
+  const lastIncident = await Incident.findOne().sort({ createdAt: -1 }).lean();
+  if (!lastIncident) return null;
+
+  return lastIncident;
+}
+
 const createIncident = async (req, res, next) => {
   try {
     const newIncident = req.body;
+    console.log(newIncident);
 
-    const player = await Player.findById(newIncident.player);
-    if (!player) throw createHttpError(StatusCodes.BAD_REQUEST, 'PLAYER_NOT_FOUND', 'Player not found');
+    const lastIncident = await findLast();
+    if (lastIncident) {
+      if (lastIncident.minute === newIncident.minute && lastIncident.type === newIncident.type) {
+        throw createHttpError(StatusCodes.BAD_REQUEST, 'INCIDENT_ALREADY_REGISTERED', 'Incident already registered');
+      }
+    }
+
+    if (newIncident.player) {
+      const player = await Player.findById(newIncident.player);
+      if (!player) throw createHttpError(StatusCodes.BAD_REQUEST, 'PLAYER_NOT_FOUND', 'Player not found');
+
+      newIncident.player = {
+        id: player._id,
+        firstName: player.firstName,
+        lastName: player.lastName
+      }
+    }
+
+    if (newIncident.assistance) {
+      const assistant = await Player.findById(newIncident.assistance);
+      if (!assistant) throw createHttpError(StatusCodes.BAD_REQUEST, 'ASSISTANT_NOT_FOUND', 'Assistant not found');
+
+      newIncident.assistance = {
+        id: assistant._id,
+        firstName: assistant.firstName,
+        lastName: assistant.lastName
+      }
+    }
+
+    if (newIncident.type === 'substitution') {
+      const playerIn = await Player.findById(newIncident.replacementIn);
+      if (!playerIn) throw createHttpError(StatusCodes.BAD_REQUEST, 'PLAYER_NOT_FOUND', 'Replacement IN not found');
+
+      const playerOut = await Player.findById(newIncident.replacementOut);
+      if (!playerOut) throw createHttpError(StatusCodes.BAD_REQUEST, 'PLAYER_NOT_FOUND', 'Replacement OUT not found');
+
+      newIncident.replacementIn = {
+        id: playerIn._id,
+        firstName: playerIn.firstName,
+        lastName: playerIn.lastName
+      }
+      newIncident.replacementOut = {
+        id: playerOut._id,
+        firstName: playerOut.firstName,
+        lastName: playerOut.lastName
+      }
+    }
 
     const team = await Team.findById(newIncident.team);
     if (!team) throw createHttpError(StatusCodes.BAD_REQUEST, 'TEAM_NOT_FOUND', 'Team not found');
@@ -21,12 +74,6 @@ const createIncident = async (req, res, next) => {
       code: team.code
     };
 
-    newIncident.player = {
-      id: player._id,
-      firstName: player.firstName,
-      lastName: player.lastName
-    }
-
     await Incident.create(newIncident);
 
     res.status(200).json({ mensaje: 'Incidencia registrada correctamente' });
@@ -36,5 +83,16 @@ const createIncident = async (req, res, next) => {
   }
 };
 
+const getIncidents = async (req, res, next) => {
+  try {
+    const { match } = req.params;
 
-module.exports = { createIncident };
+    const incidents = await Incident.find({ match }).lean();
+    res.status(200).json({ incidents});
+  } catch (error) {
+    next(error);
+  }
+}
+
+
+module.exports = { createIncident, getIncidents };
--- src/incidents/validator.js
+++ src/incidents/validator.js
@@ -6,11 +6,12 @@ const incidentRules = [
     body('detail').optional().isString(),
     body('origin').optional().isString(),
     body('minute').isInt({ min: 1 }).withMessage('Minute must be a positive integer'),
-    body('player').optional().isMongoId(),
+    body('player').optional(),
     body('team').optional().isMongoId(),
     body('reason').optional().isString(),
     body('replacementIn').optional().isString(),
-    body('replacementOut').optional().isString()
+    body('replacementOut').optional().isString(),
+    body('assistance').optional().isString()
 ];
 
 module.exports = {
